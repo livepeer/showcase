4b0670d0bb82945b978c345d794e19290439b420
Evan Mullins <evancmullins@gmail.com>
2024-12-05 11:58:09 -0600

    added geo tracking with free service TODO: add prod geo service

--- apps/app/lib/track.ts
+++ apps/app/lib/track.ts
@@ -12,15 +12,15 @@ const track = async (
     distinct_id: userUUID,
     $user_id: userUUID,
     $browser: navigator.userAgent,
-    $browser_version: navigator.appVersion,
+    $browser_version: navigator.userAgent.match(/(?:Version|Chrome|Firefox|Safari|Opera|Edge|IE)\/?\s*(\d+)/)?.[1] || '',
     $current_url: window.location.href,
-    $device: navigator.platform,
+    $device: navigator.userAgent.match(/\((.*?)\)/)?.[1] || 'Unknown Device',
     $device_id: navigator.userAgent,
     $initial_referrer: document.referrer ? document.referrer : undefined,
     $initial_referring_domain: document.referrer
       ? new URL(document.referrer).hostname
       : undefined,
-    $os: navigator.platform,
+    $os: navigator.userAgent.match(/\(([^)]+)\)/)?.[1]?.split(';')[0] || 'Unknown OS',
     $screen_height: window.screen.height,
     $screen_width: window.screen.width,
   };
@@ -28,8 +28,15 @@ const track = async (
     ...eventProperties,
     ...additionalProperties,
   };
+  console.log(
+    "Sending to mixpanel:",
+    JSON.stringify({
+      event: eventName,
+      event_properties: eventProperties,
+      properties: properties,
+    })
+  );
   try {
-   
     const response = await fetch(`/api/mixpanel`, {
       method: "POST",
       headers: {
