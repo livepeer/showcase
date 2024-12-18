5df83c42144420f798be90b256db9cf42621ead3
Hunter Hillman <hunterhillman@hunters-mbp-109.lan>
2024-12-17 17:34:07 -0800

    revert

--- apps/app/components/header/user.tsx
+++ apps/app/components/header/user.tsx
@@ -91,7 +91,7 @@ export default function User() {
           }}
           disabled={disableLogin}
         >
-          {user?.id ? "Sign in" : "Sign up"}
+          Sign In
         </Button>
       )}
     </div>
