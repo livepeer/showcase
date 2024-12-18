3144d7f93d38bc5f7725fe2016ae3ddffefdfcca
Suhail Kakar <suhail.zahed@gmail.com>
2024-12-19 00:06:35 +0530

    ci: fix for vercel builds

--- apps/app/lib/serverEnv.ts
+++ apps/app/lib/serverEnv.ts
@@ -3,13 +3,13 @@
 import { z } from "zod";
 
 const SupabaseConfig = z.object({
-    url: z.string().url(),
-    anonKey: z.string().min(1),
-    serviceRoleKey: z.string().min(1).optional(),
+  url: z.string().url().optional(),
+  anonKey: z.string().min(1).optional(),
+  serviceRoleKey: z.string().min(1).optional(),
 });
 
 const ServerEnvironmentConfig = z.object({
-    supabase: SupabaseConfig,
+  supabase: SupabaseConfig,
 });
 
 type ServerEnvironmentConfig = z.infer<typeof ServerEnvironmentConfig>;
@@ -17,11 +17,11 @@ type ServerEnvironmentConfig = z.infer<typeof ServerEnvironmentConfig>;
 // This is the only environment configuration that is allowed to be server-only
 // by doing this, we ensure these props do not make their way to the client and expose secrets
 const serverOnlyEnvConfig = {
-    supabase: {
-        url: process.env.SUPABASE_URL,
-        anonKey: process.env.SUPABASE_ANON_KEY,
-        serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
-    },
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    anonKey: process.env.SUPABASE_ANON_KEY,
+    serviceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
+  },
 } as const;
 
 const serverOnlyConfig = ServerEnvironmentConfig.parse(serverOnlyEnvConfig);
@@ -29,16 +29,16 @@ const serverOnlyConfig = ServerEnvironmentConfig.parse(serverOnlyEnvConfig);
 export const serverConfig = async () => serverOnlyConfig;
 
 export const validateServerEnv = async () => {
-    try {
-        ServerEnvironmentConfig.parse(serverOnlyConfig);
-        console.log("Server environment configuration is valid");
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            console.error("Invalid server environment configuration:");
-            error.errors.forEach((err) => {
-                console.error(`- ${err.path.join(".")}: ${err.message}`);
-            });
-        }
-        throw new Error("Server environment validation failed");
+  try {
+    ServerEnvironmentConfig.parse(serverOnlyConfig);
+    console.log("Server environment configuration is valid");
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      console.error("Invalid server environment configuration:");
+      error.errors.forEach((err) => {
+        console.error(`- ${err.path.join(".")}: ${err.message}`);
+      });
     }
+    throw new Error("Server environment validation failed");
+  }
 };
